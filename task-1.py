807fa81772e6bd0faf21c32bbcf60914dc70bcc1
youdnchime <02230210.cst@rub.edu.bt>
2024-02-15 14:37:24 +0530

    changes to task2.py file

--- task-1.py
+++ task-1.py
@@ -1,1 +1,17 @@
 print("HI IM CHIMI")
+print('Hello World')
+print('i Love programming')
+grade=100
+if grade <= 100:
+    print("excellent")
+elif grade >= 90:
+    print("Grade: A")
+elif grade >= 80:
+    print("Grade: B")
+elif grade >=70:
+    print("Grade: c")
+elif grade >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+print("i am as cool as my programme ICE")
