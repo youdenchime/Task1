09d053c3fbf1e99ce9238595753fecce1fc5f9d1
youdnchime <02230210.cst@rub.edu.bt>
2024-02-26 13:21:21 +0530

    logical

---
+++ logical.py
@@ -0,0 +1,21 @@
+#Example 1
+x=5 
+print(x>3 and x<10) #T
+#Example 2
+y=12
+print(y>10 and y%5==0)
+#Example 1
+x=5
+print(x<3 or x>10)
+#Example 2
+y=12
+print(y<10 or y % 2==0)
+#Example1
+x=5
+print(not(x>3 and x<10))# False 
+#Example2
+y=12
+print(not(y>10 and y % 5==0))
+#First Try
+x=20
+print(x<10 and x>=20)
---
+++ task 2.py
@@ -0,0 +1,1 @@
+print("i am as cool as my programme ICE")
